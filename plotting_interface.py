e283f8cd1ceeddbca22a6f2edc8c84b43154340d
Susanne Stole-Hentschel <susanhen@math.uio.no>
2021-04-13 09:01:30 +0200

    additional plotting functions

--- plotting_interface.py
+++ plotting_interface.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 contour_colormap = cm.coolwarm
+color_list = ['r', 'b', 'k'] #TODO make a proper one!
 
 labelsize=14
 
@@ -30,22 +31,22 @@ def plot_3d_as_2d(x, y, z, radial_filter=False):
     else:
         filt = 1
     plt.figure()
-    plt.imshow((filt*z).T, origin='lower', extent=(x[0]-dx//2, x[-1]+dx//2, y[0]-dy//2, y[-1]+dy//2, ))
+    plt.imshow((filt*z).T, origin='lower', extent=(x[0]-dx//2, x[-1]+dx//2, y[0]-dy//2, y[-1]+dy//2, ), aspect='auto')
     
 def plot_kx_ky_coeffs(kx, ky, coeffs, radial_filter=False):     
     plot_3d_as_2d(kx, ky, coeffs, radial_filter)    
     plt.xlabel(r'$k_x~[\mathrm{rad~m}^{-1}]$') 
-    plt.xlabel(r'$k_y~[\mathrm{rad~m}^{-1}]$')
+    plt.ylabel(r'$k_y~[\mathrm{rad~m}^{-1}]$')
      
 def plot_kx_ky_spec(kx, ky, spec, radial_filter=False):    
     plot_3d_as_2d(kx, ky, spec, radial_filter)
     plt.xlabel(r'$k_x~[\mathrm{rad~m}^{-1}]$') 
-    plt.xlabel(r'$k_y~[\mathrm{rad~m}^{-1}]$')
+    plt.ylabel(r'$k_y~[\mathrm{rad~m}^{-1}]$')
 
 def plot_k_w_spec(k, w, spec, disp_filter=False):  
     plot_3d_as_2d(k, w, spec, disp_filter)    
     plt.xlabel(r'$k~[\mathrm{rad~m}^{-1}]$') 
-    plt.xlabel(r'$\omega~[\mathrm{rad~Hz}]$')
+    plt.ylabel(r'$\omega~[\mathrm{rad~Hz}]$')
 
 def plot_wavenumber_spec(k, spec, scaled=True, k_cut_off=None):
     plot_wavenumber_specs([k], [spec], scaled, None, k_cut_off)
@@ -181,13 +182,78 @@ def plot_surfaces_along_y_at_pos(surface_list, x_pos, y_label=r'$y~[\mathrm{m}]$
         plt.plot(surf.y, surf.eta[where,:], label=surf.name)
     plt.xlabel(y_label)
     plt.ylabel(z_label)
-    plt.legend()    
+    plt.legend() 
+
+def plot_disp_rel_kx_ky(w, Ux, Uy, h):
+    k_disp_rel = w**2/9.81
+    kx_fine = np.linspace(-k_disp_rel, k_disp_rel, 300)
+    # plot positive ky
+    print('Warning: not fully implemented, current and shallow water not included')
+    ky_disp_rel_pos =  np.sqrt(k_disp_rel**2 - kx_fine**2)
+    plt.plot(kx_fine, ky_disp_rel_pos, 'w')
+    # plot negative ky
+    ky_disp_rel_neg =  -np.sqrt(k_disp_rel**2 - kx_fine**2)
+    plt.plot(kx_fine, ky_disp_rel_neg, 'w')
+
+
+
+def plot_disp_shell(axes, h, z, Ux, Uy, label='', plot_type='surf'):
+    g = 9.81
+    alpha = 0.5 # value that defines opacity in plot
+    dk = 0.005
+    k = np.arange(0.01, 0.35, dk)
+    dtheta=0.05
+    theta=np.arange(-np.pi, np.pi+dtheta, dtheta)
+    kk, th = np.meshgrid(k, theta, indexing='ij')
+    Uxk = 2*kk*np.sum(Ux*np.exp(np.outer(2*kk,z)), axis=1).reshape(kk.shape)
+    Uyk = 2*kk*np.sum(Uy*np.exp(np.outer(2*kk,z)), axis=1).reshape(kk.shape)
+    ww = kk*(np.cos(th)*Uxk + np.sin(th)*Uyk) + np.sqrt(kk*g*np.tanh(kk*h))
+    kx = kk*np.cos(th)
+    ky = kk*np.sin(th)
+    if plot_type=='surf':
+        axes.plot_surface(kx, ky, ww, alpha=alpha, label=label)
+    elif plot_type=='contour':
+        plt.contour(kx, ky, ww, label=label)
+
+def plot_multiple_disp_rel(h, z, Ux_list, Uy_list, label_list, plot_type='surf'):
+    fig = plt.figure()
+    axes = fig.gca(projection='3d')
+    for i in range(0, len(Ux_list)):
+        plot_disp_shell(axes, h, z, Ux_list[i], Uy_list[i], label_list[i], plot_type)
+    plt.legend()
+
+def plot_disp_rel_at(at_w, h, z, Ux, Uy, color):
+    g = 9.81
+    dk = 0.005
+    k = np.arange(0.01, 0.7, dk)
+    dtheta=0.05
+    theta=np.arange(-np.pi, np.pi+dtheta, dtheta)
+    kk, th = np.meshgrid(k, theta, indexing='ij')
+    Uxk = 2*kk*np.sum(Ux*np.exp(np.outer(2*kk,z)), axis=1).reshape(kk.shape)
+    Uyk = 2*kk*np.sum(Uy*np.exp(np.outer(2*kk,z)), axis=1).reshape(kk.shape)
+    ww = kk*(np.cos(th)*Uxk + np.sin(th)*Uyk) + np.sqrt(kk*g*np.tanh(kk*h))
+    kx = kk*np.cos(th)
+    ky = kk*np.sin(th)
+    CS = plt.contour(kx, ky, ww, levels=[at_w], colors=color)
+    return CS
+
+def plot_multiple_disp_rel_at(at_w, h, z, Ux_list, Uy_list, label_list, plot_type='surf'):
+    plt.figure()
+    lines = []
+    for i in range(0, len(Ux_list)):
+        CS = plot_disp_rel_at(at_w, h, z, Ux_list[i], Uy_list[i], color_list[i])
+        lines.append(CS.collections[0])    
+    plt.legend(lines, label_list)
+
 
 def savefig(fn, tight=True):
     if tight:
         plt.savefig(fn, bbox_inches='tight')
     else:
         plt.savefig(fn)
+
+def figure():
+    plt.figure()
 
 def show():
     plt.show()
