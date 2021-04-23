8d68acfbdc64fb3045a831f6a34c7eaf5e4c70a6
Susanne Stole-Hentschel <susanhen@math.uio.no>
2021-04-23 11:00:20 +0200

    update of plotting of dispersion relation

--- plotting_interface.py
+++ plotting_interface.py
@@ -23,7 +23,7 @@ def plot_3d_surface(x, y, z, radial_filter=False):
     x_mesh, y_mesh = np.meshgrid(x, y, indexing='ij')
     axes.plot_surface(x_mesh, y_mesh, (filt*z), cmap=cm.coolwarm)
 
-def plot_3d_as_2d(x, y, z, radial_filter=False):
+def plot_3d_as_2d(x, y, z, radial_filter=False, extent=None):
     dx = x[1]-x[0]
     dy = y[1]-y[0]
     if radial_filter:
@@ -31,27 +31,30 @@ def plot_3d_as_2d(x, y, z, radial_filter=False):
     else:
         filt = 1
     plt.figure()
-    plt.imshow((filt*z).T, origin='lower', extent=(x[0]-dx//2, x[-1]+dx//2, y[0]-dy//2, y[-1]+dy//2, ), aspect='auto')
+    plt.imshow((filt*z).T, origin='lower', extent=(x[0]-dx//2, x[-1]+dx//2, y[0]-dy//2, y[-1]+dy//2 ), aspect=1)
+    if not extent is None:
+        plt.xlim(extent[0], extent[1])
+        plt.ylim(extent[2], extent[3])
     
-def plot_kx_ky_coeffs(kx, ky, coeffs, radial_filter=False):     
-    plot_3d_as_2d(kx, ky, coeffs, radial_filter)    
+def plot_kx_ky_coeffs(kx, ky, coeffs, radial_filter=False, extent=None):     
+    plot_3d_as_2d(kx, ky, coeffs, radial_filter, extent)    
     plt.xlabel(r'$k_x~[\mathrm{rad~m}^{-1}]$') 
     plt.ylabel(r'$k_y~[\mathrm{rad~m}^{-1}]$')
      
-def plot_kx_ky_spec(kx, ky, spec, radial_filter=False):    
-    plot_3d_as_2d(kx, ky, spec, radial_filter)
+def plot_kx_ky_spec(kx, ky, spec, radial_filter=False, extent=None):    
+    plot_3d_as_2d(kx, ky, spec, radial_filter, extent)
     plt.xlabel(r'$k_x~[\mathrm{rad~m}^{-1}]$') 
     plt.ylabel(r'$k_y~[\mathrm{rad~m}^{-1}]$')
 
-def plot_k_w_spec(k, w, spec, disp_filter=False):  
-    plot_3d_as_2d(k, w, spec, disp_filter)    
+def plot_k_w_spec(k, w, spec, disp_filter=False, extent=None):  
+    plot_3d_as_2d(k, w, spec, disp_filter, extent)    
     plt.xlabel(r'$k~[\mathrm{rad~m}^{-1}]$') 
     plt.ylabel(r'$\omega~[\mathrm{rad~Hz}]$')
 
-def plot_wavenumber_spec(k, spec, scaled=True, k_cut_off=None):
-    plot_wavenumber_specs([k], [spec], scaled, None, k_cut_off)
+def plot_wavenumber_spec(k, spec, scaled=True, k_cut_off=None, extent=None):
+    plot_wavenumber_specs([k], [spec], scaled, None, k_cut_off, extent)
 
-def plot_wavenumber_specs(k_list, spec_list, scaled=True, labels=None, k_cut_off=None):
+def plot_wavenumber_specs(k_list, spec_list, scaled=True, labels=None, k_cut_off=None, extent=None):
     plt.figure()
     for i in range(0, len(k_list)):
         k = k_list[i]
@@ -75,9 +78,12 @@ def plot_wavenumber_specs(k_list, spec_list, scaled=True, labels=None, k_cut_off
         plt.ylabel(r'$F(k)~[\mathrm{m}^3]$')
     if not labels is None:
         plt.legend()
+    if not extent is None:
+        plt.xlim(extent[0], extent[1])
+        plt.ylim(extent[2], extent[3])
 
 
-def plot_ang_frequency_specs(w_list, spec_list, scaled=True, labels=None, w_cut_off=None):
+def plot_ang_frequency_specs(w_list, spec_list, scaled=True, labels=None, w_cut_off=None, extent=None):
     plt.figure()
     for i in range(0, len(w_list)):
         w = w_list[i]
@@ -99,12 +105,15 @@ def plot_ang_frequency_specs(w_list, spec_list, scaled=True, labels=None, w_cut_
         plt.ylabel(r'$F(\omega)/\max(F(\omega))$')
     else:
         plt.ylabel(r'$F(\omega)~[\mathrm{m}^2/\mathrm{Hz}]$')   
+    if not extent is None:
+        plt.xlim(extent[0], extent[1])
+        plt.ylim(extent[2], extent[3])
 
-def plot_ang_frequency_spec(w, spec, scaled=True, w_cut_off=None):
-    plot_ang_frequency_specs([w], [spec], scaled, None, w_cut_off)
+def plot_ang_frequency_spec(w, spec, scaled=True, w_cut_off=None, extent=None):
+    plot_ang_frequency_specs([w], [spec], scaled, None, w_cut_off, extent)
 
 
-def plot_frequency_specs(f_list, spec_list, scaled=True, labels=None, f_cut_off=None):
+def plot_frequency_specs(f_list, spec_list, scaled=True, labels=None, f_cut_off=None, extent=None):
     plt.figure()
     for i in range(0, len(f_list)):
         f = f_list[i]
@@ -125,43 +134,49 @@ def plot_frequency_specs(f_list, spec_list, scaled=True, labels=None, f_cut_off=
     if scaled:
         plt.ylabel(r'$F(f)/\max(F(f))$')
     else:
-        plt.ylabel(r'$F(f)~[\mathrm{m}^2/\mathrm{Hz}]$')   
+        plt.ylabel(r'$F(f)~[\mathrm{m}^2/\mathrm{Hz}]$') 
+    if not extent is None:
+        plt.xlim(extent[0], extent[1])
+        plt.ylim(extent[2], extent[3])  
 
-def plot_frequency_spec(f, spec, scaled=True, f_cut_off=None):        
-    plot_frequency_specs([f], [spec], scaled, None, f_cut_off)    
+def plot_frequency_spec(f, spec, scaled=True, f_cut_off=None, extent=None):        
+    plot_frequency_specs([f], [spec], scaled, None, f_cut_off, extent)   
     
         
-def plot_contours(x, y, z, radial_filter=False, levels=None, z_label=None):
-    dx = x[1]-x[0]
-    dy = y[1]-y[0]
+def plot_contours(x, y, z, radial_filter=False, levels=None, z_label=None, extent=None):
     if radial_filter:
         filt = get_radial_filter(x, y)
     else:
         filt = 1
     fig = plt.figure()
     if levels==None:
-        CS = plt.contour((filt*z).T, origin='lower', extent=(x[0]-dx//2, x[-1]+dx//2, y[0]-dy//2, y[-1]+dy//2, ))
+        CS = plt.contour(x, y, (filt*z).T, origin='lower')    
     else:
-        CS = plt.contour((filt*z).T, levels, origin='lower', extent=(x[0]-dx//2, x[-1]+dx//2, y[0]-dy//2, y[-1]+dy//2, ))    
+        CS = plt.contour(x, y, (filt*z).T, levels, origin='lower')    
     cbar = fig.colorbar(CS, shrink=0.8)
     if z_label!=None:
         cbar.ax.set_ylabel(z_label, size=labelsize, labelpad=-40, y=1.15, rotation=0)
+    if not extent is None:
+        plt.xlim(extent[0], extent[1])
+        plt.ylim(extent[2], extent[3])
     
-def plot_contourf(x, y, z, radial_filter=False, levels=None, z_label=None):
-    dx = x[1]-x[0]
-    dy = y[1]-y[0]
+def plot_contourf(x, y, z, radial_filter=False, levels=None, z_label=None, extent=None):
+    
     if radial_filter:
         filt = get_radial_filter(x, y)
     else:
         filt = 1
     fig = plt.figure()
     if levels==None:
-        CF = plt.contourf((filt*z).T, origin='lower', extent=(x[0]-dx//2, x[-1]-dx//2, y[0]-dy//2, y[-1]-dy//2, ), cmap=contour_colormap)
+        CF = plt.contourf(x, y, (filt*z).T, origin='lower', cmap=contour_colormap)
     else:
-        CF = plt.contourf((filt*z).T, levels, origin='lower', extent=(x[0]-dx//2, x[-1]+dx//2, y[0]-dy//2, y[-1]+dy//2, ), cmap=contour_colormap)
+        CF = plt.contourf(x, y, (filt*z).T, levels, origin='lower', cmap=contour_colormap)
     cbar = fig.colorbar(CF, shrink=0.8, panchor=(1., 0.4))
     if z_label!=None:
         cbar.ax.set_ylabel(z_label, size=labelsize, labelpad=-20, y=1.15, rotation=0)
+    if not extent is None:
+        plt.xlim(extent[0], extent[1])
+        plt.ylim(extent[2], extent[3])
 
 
 def plot_surfaces_along_y_at_random(surface_list, y_label=r'$y~[\mathrm{m}]$', z_label=r'$\eta~[\mathrm{m}]$'):
@@ -184,7 +199,7 @@ def plot_surfaces_along_y_at_pos(surface_list, x_pos, y_label=r'$y~[\mathrm{m}]$
     plt.ylabel(z_label)
     plt.legend() 
 
-def plot_disp_rel_kx_ky(w, Ux, Uy, h):
+def plot_disp_rel_kx_ky(w, h):
     k_disp_rel = w**2/9.81
     kx_fine = np.linspace(-k_disp_rel, k_disp_rel, 300)
     # plot positive ky
@@ -197,17 +212,16 @@ def plot_disp_rel_kx_ky(w, Ux, Uy, h):
 
 
 
-def plot_disp_shell(axes, h, z, Ux, Uy, label='', plot_type='surf'):
+def plot_disp_shell(axes, h, z, U, psi, label='', plot_type='surf'):
     g = 9.81
     alpha = 0.5 # value that defines opacity in plot
     dk = 0.005
     k = np.arange(0.01, 0.35, dk)
     dtheta=0.05
-    theta=np.arange(-np.pi, np.pi+dtheta, dtheta)
+    theta=np.arange(0, 2*np.pi+dtheta, dtheta)
     kk, th = np.meshgrid(k, theta, indexing='ij')
-    Uxk = 2*kk*np.sum(Ux*np.exp(np.outer(2*kk,z)), axis=1).reshape(kk.shape)
-    Uyk = 2*kk*np.sum(Uy*np.exp(np.outer(2*kk,z)), axis=1).reshape(kk.shape)
-    ww = kk*(np.cos(th)*Uxk + np.sin(th)*Uyk) + np.sqrt(kk*g*np.tanh(kk*h))
+    U_eff = 2*kk*np.sum(U*np.exp(np.outer(2*kk,z)), axis=1).reshape(kk.shape)
+    ww = kk*U_eff*np.cos(theta-psi) + np.sqrt(kk*g*np.tanh(kk*h))
     kx = kk*np.cos(th)
     ky = kk*np.sin(th)
     if plot_type=='surf':
@@ -215,33 +229,40 @@ def plot_disp_shell(axes, h, z, Ux, Uy, label='', plot_type='surf'):
     elif plot_type=='contour':
         plt.contour(kx, ky, ww, label=label)
 
-def plot_multiple_disp_rel(h, z, Ux_list, Uy_list, label_list, plot_type='surf'):
+def plot_multiple_disp_rel(h, z, U_list, psi_list, label_list, plot_type='surf'):
     fig = plt.figure()
     axes = fig.gca(projection='3d')
-    for i in range(0, len(Ux_list)):
-        plot_disp_shell(axes, h, z, Ux_list[i], Uy_list[i], label_list[i], plot_type)
+    for i in range(0, len(U_list)):
+        plot_disp_shell(axes, h, z, U_list[i], psi_list[i], label_list[i], plot_type)
     plt.legend()
 
-def plot_disp_rel_at(at_w, h, z, Ux, Uy, color):
+def plot_disp_rel_at(at_w, h, z, U, psi, color, extent=None):
+    '''
+    psi: angle between waves and current
+    '''
     g = 9.81
     dk = 0.005
     k = np.arange(0.01, 0.7, dk)
     dtheta=0.05
-    theta=np.arange(-np.pi, np.pi+dtheta, dtheta)
+    theta=np.arange(0, 2*np.pi+dtheta, dtheta)
     kk, th = np.meshgrid(k, theta, indexing='ij')
-    Uxk = 2*kk*np.sum(Ux*np.exp(np.outer(2*kk,z)), axis=1).reshape(kk.shape)
-    Uyk = 2*kk*np.sum(Uy*np.exp(np.outer(2*kk,z)), axis=1).reshape(kk.shape)
-    ww = kk*(np.cos(th)*Uxk + np.sin(th)*Uyk) + np.sqrt(kk*g*np.tanh(kk*h))
+    U_eff = 2*kk*np.sum(U*np.exp(np.outer(2*kk,z)), axis=1).reshape(kk.shape)
+    ww = kk*U_eff*np.cos(th-psi) + np.sqrt(kk*g*np.tanh(kk*h))
     kx = kk*np.cos(th)
     ky = kk*np.sin(th)
-    CS = plt.contour(kx, ky, ww, levels=[at_w], colors=color)
+    
+    CS = plt.contour(kx, ky, ww, origin='lower', levels=[at_w], colors=color)
+    plt.axes().set_aspect('equal')
+    if not extent is None:
+        plt.xlim(extent[0], extent[1])
+        plt.ylim(extent[2], extent[3])
     return CS
 
-def plot_multiple_disp_rel_at(at_w, h, z, Ux_list, Uy_list, label_list, plot_type='surf'):
+def plot_multiple_disp_rel_at(at_w, h, z, U_list, psi_list, label_list, plot_type='surf', extent=None):
     plt.figure()
     lines = []
-    for i in range(0, len(Ux_list)):
-        CS = plot_disp_rel_at(at_w, h, z, Ux_list[i], Uy_list[i], color_list[i])
+    for i in range(0, len(U_list)):
+        CS = plot_disp_rel_at(at_w, h, z, U_list[i], psi_list[i], color_list[i], extent)
         lines.append(CS.collections[0])    
     plt.legend(lines, label_list)
 
